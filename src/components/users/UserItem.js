a71c53a2fa6ac38fa9fecdd5951e5de88d73c6e6
Milank C A <milankca@gmail.com>
2022-04-17 17:18:30 +0530

    Use state as a variable in component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-constructor(){
-    super();
-    this.state = {
+    state = {
         id: 'id',
         login: 'mojombo',
         avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
         html_url: 'https://github.com/mojombo'
     }
-}
 
   render() {
     return <div className='card text-center'>
