ecc9fbfb2a8bed813fdd7535f3f8fb0b1c85d348
Milank C A <milankca@gmail.com>
2022-04-17 17:16:58 +0530

    Add UserItem component - State inside constructor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import Navbar from './components/layout/Navbar'
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem';
 
 class App extends Component  {
 
@@ -9,6 +10,7 @@ class App extends Component  {
     return (
       <div className="App">
        <Navbar />
+       <UserItem />
       </div>
     ); 
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+constructor(){
+    super();
+    this.state = {
+        id: 'id',
+        login: 'mojombo',
+        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo'
+    }
+}
+
+  render() {
+    return <div className='card text-center'>
+        <img src={this.state.avatar_url} className = "round-img" 
+        style = {{width: '60px'}}/>
+       <h3>{this.state.login}</h3>
+
+       <div>
+           <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+               more
+           </a>
+       </div>
+    </div>
+  }
+}
+
+export default UserItem
